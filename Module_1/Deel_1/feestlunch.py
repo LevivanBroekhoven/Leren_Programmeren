d0516e8e2f9e6ad6bca2047e52ba2a47f6eb4ca9
LevivanBroekhoven <99076037@mydavinci.nl>
2023-09-25 12:00:29 +0200

    feestlunch en speelhal opdracht

---
+++ Module_1/Deel_1/feestlunch.py
@@ -0,0 +1,8 @@
+crosaint = 0.39*17
+stokbrood = 2.78*2
+kortingbon = 0.50*3
+
+antwoord = crosaint + stokbrood - kortingbon
+print (antwoord)
+
+
---
+++ Module_1/Deel_1/speelhal_dag.py
@@ -0,0 +1,9 @@
+entre = 7.45
+antwoord = 45 / 5 
+
+PP = antwoord * 0.37
+
+totale_koste = PP + entre
+meerdere = totale_koste * 4
+print("prijs per persoon is" ,round(totale_koste,2),'euro')
+print("prijs voor 4 personen is" ,round(meerdere,2),'euro')
