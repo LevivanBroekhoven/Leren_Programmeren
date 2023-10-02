a0be24a032e610d1f8cbe112e56cfe8761428115
LevivanBroekhoven <99076037@mydavinci.nl>
2023-10-02 13:17:09 +0200

    Updated Feestlunch speelhal

--- Module_1/Deel_2/feestlunch.py
+++ Module_1/Deel_2/feestlunch.py
@@ -2,13 +2,14 @@ crosaint = int(input("hoeveel crosaintjes wil je? "))
 stokbrood = int(input("hoeveel stokbroodjes wil je? "))
 kortingbon = int(input("hoeveel kortingsbonnen heb je? "))
 
-CROSAINTP = 0.39
-STOKBROODP = 2.78
-KORTINGBON = 0.50
+CROSAINTP = 39
+STOKBROODP = 278
+KORTINGBON = 50
 
 crosaintprijs = CROSAINTP * crosaint
 stokbroodprijs = STOKBROODP *stokbrood
 kortingbonprijs = KORTINGBON * kortingbon
 
-antwoord = crosaintprijs + stokbroodprijs - kortingbonprijs
-print ("De feestlunch kost je bij de bakker",round(antwoord,2),"euro voor",crosaint,"crossaints en",stokbrood, "stokbroden als de",kortingbon,"kortingsbonnen nog geldig zijn!.")
+antwoordC = crosaintprijs + stokbroodprijs - kortingbonprijs
+antwoordE = antwoordC /100
+print ("De feestlunch kost je bij de bakker",round(antwoordE,2),"euro voor",crosaint,"crossaints en",stokbrood, "stokbroden als de",kortingbon,"kortingsbonnen nog geldig zijn!.")
