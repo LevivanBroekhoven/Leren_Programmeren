b71bda3377ca25d075aa15ff9ecae37dfcb1d2cb
LevivanBroekhoven <99076037@mydavinci.nl>
2023-10-02 14:29:47 +0200

    pizza calc

--- Module_1/Deel_2/pizzaCalculator.py
+++ Module_1/Deel_2/pizzaCalculator.py
@@ -1,30 +1,24 @@
 #Levi van Broekhoven Opdracht Pizza Calculator
-small = 6
-medium = 8
-large = 15
+prijzen = {"small": 8.99,"medium": 11.99,"large": 14.99}
 
-PizzaGroote = input("wat voor grote pizza wilt u? (small medium large) ")
-PizzaAantal = int(input("hoeveel pizza's wilt u? "))
+PizzaAantalsmall = int(input(f"hoeveel small pizza's wilt u? "))
+PizzaAantalmedium = int(input(f"hoeveel medium pizza's wilt u? "))
+PizzaAantallarge = int(input(f"hoeveel large pizza's wilt u? "))
 
-if PizzaGroote == ("small"):
-    prijs = PizzaAantal * small
-    print("----------------------------------------------------")
-    print(f"| Aantal pizza's :  {PizzaAantal}                  ")
-    print(f"| Groote pizza   :  {PizzaGroote}                  ")
-    print(f"| Prijs          :  {prijs} euro                   ")
-    print("----------------------------------------------------")
-if PizzaGroote == ("medium"):
-    prijs = PizzaAantal * medium
-    print("----------------------------------------------------")
-    print(f"| Aantal pizza's :  {PizzaAantal}                  ")
-    print(f"| Groote pizza   :  {PizzaGroote}                  ")
-    print(f"| Prijs          :  {prijs} euro                   ")
-    print("----------------------------------------------------")
-if PizzaGroote == ("large"):
-    prijs = PizzaAantal * large
-    print("----------------------------------------------------")
-    print(f"| Aantal pizza's :  {PizzaAantal}                  ")
-    print(f"| Groote pizza   :  {PizzaGroote}                  ")
-    print(f"| Prijs          :  {prijs} euro                   ")
-    print("----------------------------------------------------")
-   
+prijs_small  = PizzaAantalsmall   * prijzen["small"]
+prijs_medium = PizzaAantalmedium  * prijzen["medium"]
+prijs_large  = PizzaAantallarge   * prijzen["large"]
+
+totaalprijs = prijs_small + prijs_medium + prijs_large
+totaalaantal = PizzaAantalsmall + PizzaAantalmedium + PizzaAantallarge
+
+print(" ------------------------------------------")
+print("                  Bonnetje                 ")
+print(" ------------------------------------------")
+print("                                           ")
+print(f"  Aantal pizza's {totaalaantal}           ")
+print(f"  Prijs {totaalprijs} euro                ")
+print("                                           ")
+print(" -------------------------------------------")
+
+            
