64f0570705aff1bdf396738805a87a8bf4e2a801
LevivanBroekhoven <99076037@mydavinci.nl>
2023-10-02 12:20:31 +0200

    Update pizzaCalculator.py

--- Module_1/Deel_2/pizzaCalculator.py
+++ Module_1/Deel_2/pizzaCalculator.py
@@ -1,17 +1,30 @@
-small = 1
-medium = 2
-large = 3
+#Levi van Broekhoven Opdracht Pizza Calculator
+small = 6
+medium = 8
+large = 15
 
 PizzaGroote = input("wat voor grote pizza wilt u? (small medium large) ")
 PizzaAantal = int(input("hoeveel pizza's wilt u? "))
 
 if PizzaGroote == ("small"):
     prijs = PizzaAantal * small
-    print(f" Uw {PizzaAantal } {PizzaGroote} Pizza's kosten {prijs} euro" )
+    print("----------------------------------------------------")
+    print(f"| Aantal pizza's :  {PizzaAantal}                  ")
+    print(f"| Groote pizza   :  {PizzaGroote}                  ")
+    print(f"| Prijs          :  {prijs} euro                   ")
+    print("----------------------------------------------------")
 if PizzaGroote == ("medium"):
     prijs = PizzaAantal * medium
-    print(f" Uw {PizzaAantal } {PizzaGroote} Pizza's kosten {prijs} euro" )
+    print("----------------------------------------------------")
+    print(f"| Aantal pizza's :  {PizzaAantal}                  ")
+    print(f"| Groote pizza   :  {PizzaGroote}                  ")
+    print(f"| Prijs          :  {prijs} euro                   ")
+    print("----------------------------------------------------")
 if PizzaGroote == ("large"):
     prijs = PizzaAantal * large
-    print(f" Uw {PizzaAantal } {PizzaGroote} Pizza's kosten {prijs} euro" )
+    print("----------------------------------------------------")
+    print(f"| Aantal pizza's :  {PizzaAantal}                  ")
+    print(f"| Groote pizza   :  {PizzaGroote}                  ")
+    print(f"| Prijs          :  {prijs} euro                   ")
+    print("----------------------------------------------------")
    
