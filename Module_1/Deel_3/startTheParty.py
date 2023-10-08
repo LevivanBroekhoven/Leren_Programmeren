9aa267590db1fa1443efbb1267c8a98cc879b875
LevivanBroekhoven <99076037@mydavinci.nl>
2023-10-08 11:42:31 +0200

    Update startTheParty.py

--- Module_1/Deel_3/startTheParty.py
+++ Module_1/Deel_3/startTheParty.py
@@ -1,19 +1,9 @@
-<<<<<<< Updated upstream
 drank = True
 chips = False
 mijn_naam = ("Levi")
 SLB_naam = ("Eugene")
 gastheer = input("Naam gastheer ")
 gasten = int(input("Hoeveel gasten? "))
-=======
-gastheer = True
-gasten = False
-drank = True
-chips = False
-jouw_naam = "Levi"
-slber_naam = "Oorschot"
-naam_gastheer = input("naam gastheer ")
->>>>>>> Stashed changes
 
 if gastheer == mijn_naam:
     print('No Party')
@@ -25,17 +15,7 @@ elif gasten < 4 or gasten > 20:
     print('No Party')
 elif gasten and drank and chips == True:
     print('Start the Party')
-elif naam_gastheer is not  jouw_naam or slber_naam:
-    gastheer = False
 elif gastheer or gasten and chips and drank == True:
     print("Start the Party")
-<<<<<<< Updated upstream
-=======
-elif gastheer and drank == True:
-    print("Start the Party")
-
-elif gastheer == False and naam_gastheer == ("Levi"):
-    print("No Party? ")
->>>>>>> Stashed changes
 else:
     print('No Party')
