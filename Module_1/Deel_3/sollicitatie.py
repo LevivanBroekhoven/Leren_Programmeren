7474fe926d268a5078fd27c9fd0c841216a7988c
LevivanBroekhoven <99076037@mydavinci.nl>
2023-10-24 09:31:03 +0200

    Update sollicitatie.py

--- Module_1/Deel_3/sollicitatie.py
+++ Module_1/Deel_3/sollicitatie.py
@@ -1,7 +1,7 @@
 Naam = input("Wat is uw naam: ")
-Gewicht = input("Weegt u zwaarder dan 90kg en lichter dan 120kg: ")
+Gewicht = int(input("Wat is uw gewicht: "))
 Hoed = input("Heeft u een hoge hoed: ")
-lengte = input("Ben u langer dan 150cm en korter dan 220cm: ")
+lengte = int(input("Wat is uw lengte: "))
 rijbewijs = input("Heeft u een vrachtwagen rijbewijs? ")
 certificaat = input("Heeft u het certificaat Overleven met gevaarlijk persoon: ")
 certificaat2 = int(input("Hoeveel jaar praktijkervaring met dieren-dressuur heeft u? "))
@@ -13,23 +13,37 @@ werknemers = int(input("Hoeveel werknemers had u in dienst? "))
 geslacht = input("bent u man of vrouw? of anders ")
 ResultaatSollicitatie = "Wel"
 
-if Gewicht == "nee" or Hoed == "nee" or lengte == "nee" or certificaat == "nee" or rijbewijs == "nee" or diploma == "nee":
+MIN_GEWICHT = 90
+MAX_GEWICHT = 120
+
+MIN_LENGTE = 150
+MAX_LENGTE = 220
+
+SNOR_MIN = 10
+
+HAAR_MIN = 20
+
+LACH_MIN = 10
+
+
+
+if Gewicht < MIN_GEWICHT or Gewicht > MAX_GEWICHT or Hoed == "nee" or lengte < MIN_LENGTE or lengte > MAX_LENGTE or certificaat == "nee" or rijbewijs == "nee" or diploma == "nee":
     ResultaatSollicitatie = "Niet"
 
 elif certificaat2 < 3 or certificaat3 <= 4 and certificaat4 <= 2:
     ResultaatSollicitatie = "Niet"
 
 if geslacht == "man":
-    snor = input("Is uw snor breder dan 10cm? ")
-    if snor == "nee":
+    snor = int(input("Hoe breed is uw snor "))
+    if snor < SNOR_MIN:
         ResultaatSollicitatie = "Niet"
 elif geslacht == "vrouw":
-    haar = input("Heeft u rood krulhaar langer dan 20cm? ")
-    if haar == "nee":
+    haar = int(input("Heeft u rood krulhaar en hoe lang is dat? "))
+    if haar < HAAR_MIN or haar == "nee":
         ResultaatSollicitatie = "Niet"
 elif geslacht == "anders":
-    lach = input("Heeft u een lach breder dan 10cm? ")
-    if lach == "nee":
+    lach = int(input("Hoe breed is uw lach? "))
+    if lach < LACH_MIN:
         ResultaatSollicitatie = "Niet"
 if ondernemer <= 3 and werknemers <= 5:
     ResultaatSollicitatie = "Niet"
@@ -39,11 +53,11 @@ if ResultaatSollicitatie == "Wel":
 
 elif ResultaatSollicitatie == "Niet":
      print(f"Hallo {Naam}, u bent {ResultaatSollicitatie} aangenomen want.")
-     if Gewicht == "nee":
+     if Gewicht < MIN_GEWICHT or Gewicht > MAX_GEWICHT:
          print("U ben te licht/ Te zwaar voor ons")
      if Hoed == "nee":
          print("u had geen hoge hoed")
-     if lengte == "nee":
+     if lengte < MIN_LENGTE or lengte > MAX_LENGTE:
          print("U ben te lang/ te klein voor ons")
      if rijbewijs == "nee":
          print("U heeft geen rijbewijs")
@@ -57,9 +71,9 @@ elif ResultaatSollicitatie == "Niet":
          print("U heeft niet genoeg jaar ervaring met jongleren")
      if certificaat4 < 3:
          print("U heeft niet genoeg jaar praktijkervaring met acrobatiek")
-     if geslacht == "man" and snor == "nee":
+     if geslacht == "man" and snor < SNOR_MIN:
          print("Uw snor is kleiner dan 10cm")
-     if geslacht == "vrouw" and haar == "nee":
+     if geslacht == "vrouw" and haar < HAAR_MIN or haar == "nee":
          print("Uw haar is korter dan 20cm of niet rood")
-     if geslacht == "anders" and lach == "nee":
+     if geslacht == "anders" and lach < LACH_MIN:
          print ("Uw lach is minder breed dan 10cm")
