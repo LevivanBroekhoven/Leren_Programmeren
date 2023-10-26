548e6ce6bfaf2a068a5e8dc75166a324fe9fb23f
LevivanBroekhoven <99076037@mydavinci.nl>
2023-10-26 14:07:11 +0200

    Finished Order_Bug

---
+++ Module_1/Deel_4/order_bug.py
@@ -0,0 +1,20 @@
+def vraag_getal(aantal):
+    getal = input(f"Voer het {aantal} getal in: ")
+    getal = int(getal)
+    return getal
+
+def deel_getallen(a, b):
+    antwoord = (a / b)  
+    return antwoord
+
+getal_1 = vraag_getal("eerste")
+getal_2 = vraag_getal("tweede")
+resultaat = deel_getallen(getal_1, getal_2)
+
+if getal_2 == 0:
+    print("Kan niet delen door 0")
+else:
+    print (f"{getal_1} gedeeld door {getal_2} is gelijk aan {resultaat}".format(getal_1, getal_2, resultaat))
+    
+
+
