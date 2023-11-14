c72f481aeb5c9c8e6bc37131c2e75be189b5af02
LevivanBroekhoven <99076037@mydavinci.nl>
2023-11-14 09:56:20 +0100

    Update stuivers.py

--- Module_1/Deel_5/stuivers.py
+++ Module_1/Deel_5/stuivers.py
@@ -1,11 +1,32 @@
-def afronden(Bedrag):
-    afronding = round(Bedrag * 100 / 5) * 5 /100
-    return afronding 
+from math import *
+from test_lib import *
+
+AANTAL_CENTEN = 5
+Bedrag1 = 76.61
+Bedrag2 = 28.82
+Bedrag3 = 2.23
+
+
+def afronden(Bedrag:float,):
+    afronding = (round(Bedrag * 100 / 5) * 5 /100)
+    return '{:.2f}' .format(round(afronding, 2))
 
 
 
-Bedrag = 62.63
-afgerond_Bedrag = afronden(Bedrag)
 
-print (afgerond_Bedrag) 
 
+
+excepted = '{:.2f}' .format(round(76.60 ,2))
+calculate = afronden(Bedrag1 )
+test ("Afronding", excepted , calculate)
+
+excepted = '{:.2f}' .format(round(28.80 ,2))    
+calculate = afronden(Bedrag2 )
+test ("Afronding", excepted , calculate)
+
+excepted = '{:.2f}' .format(round(2.25 ,2))
+calculate = afronden(Bedrag3 )
+test ("Afronding", excepted , calculate)
+
+
+report()
