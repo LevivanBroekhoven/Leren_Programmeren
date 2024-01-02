f65eb03088d7b52af1e6fe1dcbb91889c3032951
LevivanBroekhoven <99076037@mydavinci.nl>
2024-01-02 16:24:49 +0100

    Opdracht 3

---
+++ Module_2/Deel_1/robotarm-python-2023-2.3/example3.py
@@ -0,0 +1,25 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 3')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
