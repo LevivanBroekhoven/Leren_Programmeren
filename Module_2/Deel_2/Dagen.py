feb77937d0bb26bd523ac8847b66fd64fbf5d0c9
LevivanBroekhoven <99076037@mydavinci.nl>
2024-01-04 12:03:17 +0100

    Dagen

---
+++ Module_2/Deel_2/Dagen.py
@@ -0,0 +1,47 @@
+# Dagentuple  =("Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag","Zondag")
+# WerkDagen = Dagentuple[:5]
+# WeekendDagen = Dagentuple[5:]
+
+
+
+# print (Dagentuple)
+# print (WerkDagen)
+# print (WeekendDagen)
+# print (Dagentuple[::-1])
+# print (WerkDagen[::-1])
+# print (WeekendDagen[::-1])
+
+Dagentuple = ("Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag")
+WerkDagen = Dagentuple[:5]
+WeekendDagen = Dagentuple[5:]
+
+
+print("Alle dagen van de week zijn:", end=" ")
+for x in Dagentuple:
+    print(x, end=", ")
+print("\n")
+
+print("De werkdagen zijn:", end=" ")
+for x in WerkDagen:
+    print(x, end=", ")
+print("\n")
+
+print("De weekenddagen zijn:", end=" ")
+for x in WeekendDagen:
+    print(x, end=", ")
+print("\n")
+
+print("Alle dagen van de week in omgekeerde volgorde zijn:", end=" ")
+for x in reversed(Dagentuple):
+    print(x, end=", ")
+print("\n")
+
+print("De werkdagen in omgekeerde volgorde zijn:", end=" ")
+for x in reversed(WerkDagen):
+    print(x, end=", ")
+print("\n")
+
+print("De weekenddagen in omgekeerde volgorde zijn:", end=" ")
+for x in reversed(WeekendDagen):
+    print(x, end=", ")
+print("\n")
