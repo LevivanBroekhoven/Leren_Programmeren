0dfc60ba614139f248e83445efa75847f52d5fe2
LevivanBroekhoven <99076037@mydavinci.nl>
2024-01-12 11:09:59 +0100

    Create speelkaarten.py

---
+++ Module_2/Deel_2/speelkaarten.py
@@ -0,0 +1,20 @@
+import random
+kleuren = ('harten', 'klaven', 'schoppen', 'ruiten')
+soorten = ("2","3","4","5","6","7","8","9","10","boer","vrouw","koning","aas")
+Deck = []
+jouw_Deck = []
+
+for a in kleuren:
+    for b in soorten:
+        Deck.append(f"{a} {b}")
+
+Deck.append("joker")
+Deck.append("joker")
+random.shuffle(Deck)
+
+for x in range(1,8):
+    jouw_Deck = Deck.pop(0)
+    print(f" Kaart {x} {jouw_Deck}")
+
+print("\n")
+print("Deck (47 kaarten):", Deck) 
