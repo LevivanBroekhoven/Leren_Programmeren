cd7f34e469786f1671da3b819f556390262f80f2
LevivanBroekhoven <99076037@mydavinci.nl>
2024-03-07 11:57:20 +0100

    Robot arm update

--- Module_2/Deel_1/robotarm-python-2023-2.3/example6.py
+++ Module_2/Deel_1/robotarm-python-2023-2.3/example6.py
@@ -7,26 +7,20 @@ robotArm.moveRight()
 robotArm.grab()
 robotArm.moveLeft()
 robotArm.drop()
-robotArm.moveRight()
-robotArm.grab()
-robotArm.moveRight()
-robotArm.drop()
-robotArm.moveLeft()
-robotArm.grab()
-robotArm.moveLeft()
-robotArm.drop()
-robotArm.moveRight()
-robotArm.grab()
-robotArm.moveRight()
-robotArm.drop()
-robotArm.moveLeft()
-robotArm.grab()
-robotArm.moveLeft()
-robotArm.drop()
-robotArm.moveRight()
-robotArm.grab()
-robotArm.moveRight()
-robotArm.drop()
+
+for x in range(3):
+    robotArm.moveRight()
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    if x < 2:
+        for y in range(1):
+            robotArm.moveLeft()
+            robotArm.grab()
+            robotArm.moveLeft()
+            robotArm.drop()
+
+
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
