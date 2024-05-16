4e29f799fc3bac3c95228ade159881f6452e638d
LevivanBroekhoven <99076037@mydavinci.nl>
2024-05-16 10:26:28 +0200

    Begin Eindopdracht M2

---
+++ Module_2/Eind_Opdracht2.py/EindOpdracht2.py
@@ -0,0 +1,17 @@
+namen_lijst = []
+lootjes_trekken = False
+
+while not lootjes_trekken:
+    naam = input("Voer een naam in: ")
+    while True:
+        if naam in namen_lijst:
+            print("Voer een nieuwe naam in")
+        else:
+            namen_lijst.append(naam)
+            break
+
+    if len(namen_lijst) >= 3:
+        lootjes_trekken = input("wilt u de lootjes trekken (j/n)") == 'j'
+
+for naam in namen_lijst:
+    print(naam)
