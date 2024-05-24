b3def3ef2453a7e56cd93b13a48ccecc327a7476
LevivanBroekhoven <99076037@mydavinci.nl>
2024-05-24 12:16:47 +0200

    commentaar voor elke functie

--- Module_3/Deel_1/rename_me.py
+++ Module_3/Deel_1/rename_me.py
@@ -1,16 +1,24 @@
 def quantum_broodrooster(stellar_broccoli:int) -> bool:
     return stellar_broccoli % 2 == 0
+# geeft de rest van de meegegeven int
+
 
 def chaos_papegaai(fantasie_platypus:str) -> str:
     betoverde_druif = fantasie_platypus.split()
     doldwaze_broccoli = betoverde_druif[::-1]
     tijdmachine_pannenkoekenmix = ' '.join(doldwaze_broccoli)
     return tijdmachine_pannenkoekenmix
+# draait de zin om
+
+
 
 def kosmische_koekjesmix(galactische_snoepjes:str) -> int:
     planetair_taartje = set(galactische_snoepjes)
     whatchamacallit = len(planetair_taartje)
     return whatchamacallit
+# Telt het aantal unieke letters in een woord
+
+
 
 def ruimte_hamsterwiel(planetair_taartje:str) -> float:
     wobbelwobbel = planetair_taartje.split()
@@ -21,8 +29,13 @@ def ruimte_hamsterwiel(planetair_taartje:str) -> float:
 
     bizarro_matrix = blork / len(wobbelwobbel)
     return bizarro_matrix
+# berekent gemiddelde woordlengte
+
+
 
 def spaghetti_spaceship(infinity_pizza:int, parallelle_tosti:int=10) -> None:
     for zwabber_krakeling in range(1, parallelle_tosti+1):
         laser_sandwich = zwabber_krakeling * infinity_pizza
         print(f'{zwabber_krakeling} x {infinity_pizza} = {laser_sandwich}')
+# doet het eerste nummer in een tafel tot de 2de nummer
+
