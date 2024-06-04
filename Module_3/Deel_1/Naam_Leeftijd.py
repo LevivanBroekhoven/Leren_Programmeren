9df255d48ef77658bb1ad17a77207ec117600c1c
LevivanBroekhoven <99076037@mydavinci.nl>
2024-06-04 09:59:04 +0200

    Create Naam_Leeftijd.py

---
+++ Module_3/Deel_1/Naam_Leeftijd.py
@@ -0,0 +1,15 @@
+def name():
+    naam = input("Wat is je naam? ")
+    return(naam)
+
+def age():
+    leeftijd = input("Wat is je leeftijd? ")
+    return(leeftijd)
+
+
+naam = name()
+leeftijd = age()
+
+
+print(f"{naam} is {leeftijd}")
+
