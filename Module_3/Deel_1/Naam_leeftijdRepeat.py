34107c575c57948705a07700dbe21df294facfbb
LevivanBroekhoven <99076037@mydavinci.nl>
2024-06-05 11:00:17 +0200

    Naam en leeftijd opdracht

---
+++ Module_3/Deel_1/Naam_leeftijdRepeat.py
@@ -0,0 +1,38 @@
+list = []
+
+
+def name():
+    naam = input("Wat is je naam? ")
+    return(naam)
+
+def age():
+    leeftijd = input("Wat is je leeftijd? ")
+    return(leeftijd)
+
+def living_place():
+    woonplaats = input("Waar woon je? ")
+    return(woonplaats)
+
+
+
+def vragen():
+    naam = name()
+    leeftijd = age()
+    woonplaats = living_place()
+    return{"naam":naam, "leeftijd":leeftijd, "Woon":woonplaats}
+
+while True: 
+    list.append(vragen())
+    doorgaan = input("nog iemand?")
+    if doorgaan == "stop":
+        break
+
+
+for x in list:
+    print(f"{x['naam']} die in {x['Woon']} woont, is {x['leeftijd']}  jaar oud")
+
+
+
+
+
+
