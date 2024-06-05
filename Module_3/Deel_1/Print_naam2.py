dfd8ead869d735634102c1bfdc791f92f50c06ca
LevivanBroekhoven <99076037@mydavinci.nl>
2024-06-05 11:34:22 +0200

    andere output zelfde bestand voor verzamelen

---
+++ Module_3/Deel_1/Print_naam2.py
@@ -0,0 +1,13 @@
+from Functions_naam import *
+list = []
+
+while True: 
+    list.append(vragen())
+    doorgaan = input("nog iemand? ")
+    if doorgaan == "stop":
+        break
+
+
+for x in list:
+    print(f"in {x['Woon']} woont,de {x['leeftijd']} jarige {x['naam']}  ")
+
