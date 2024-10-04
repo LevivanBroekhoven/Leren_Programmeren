4c3f562a2a5b90f518dbfce134af06ec40b7cd91
LevivanBroekhoven <99076037@mydavinci.nl>
2024-10-04 12:00:58 +0200

    Eindopdracht deel3

--- Module_3/EindOpdracht3/EindOpdracht3.py
+++ Module_3/EindOpdracht3/EindOpdracht3.py
@@ -1,39 +1,16 @@
+from EindOpdracht3Data import *
+from EindOpdrachtFunctions import *
+doorgaan = "ja"
 print("Welkom bij Papi Gelato! Je mag alle smaken kiezen zolang het maar vanille ijs is.")
 
-while True:
-    try:
-        bolletje = int(input("Hoeveel bolletjes wilt u? "))
-
-        if 1 <= bolletje <= 3:
-            while True:
-                hoorntjeofBakje = input(f"Wilt u deze {bolletje} bolletjes in een hoorntje of bakje? ")
-                if hoorntjeofBakje == "hoorntje":
-                    print(f"Hier is uw {bolletje} bolletjes in een hoorntje.")
-                    break
-                elif hoorntjeofBakje == "bakje":
-                    print(f"Hier is uw {bolletje} bolletjes in een bakje.")
-                    break
-                else:
-                    print("Sorry dat snap ik niet")
+while doorgaan != "nee":
+    bolletje = Vraag_bolletjes()
+    kies_horen_of_bakje(bolletje)
+    doorgaan = meerbestellen()
 
 
-        elif 4 <= bolletje <= 8:
-            print(f"Hier is uw {bolletje} bolletjes in een bakje.")
 
-        elif bolletje > 8:
-            print("Sorry, zulke grote bakken hebben we niet.")
 
-        else:
-            print("Graag een geldig aantal bolletjes in te voeren.")
 
-        jofninput = input("Wilt u nog meer bestellen? (Ja/Nee) ")
-        if jofninput == "nee":
-            print("Bedankt en tot ziens!")
-            break
-        if jofninput == "ja":
-            ()
-        else:
-            print("Sorry, dat snap ik niet.")
 
-    except ValueError:
-        print("Sorry, dat snap ik niet.")
+
---
+++ Module_3/EindOpdracht3/EindOpdrachtFunctions.py
@@ -0,0 +1,50 @@
+from EindOpdracht3Data import *
+
+
+def Vraag_bolletjes():
+    while True:
+        try:
+            bolletje = int(input("Hoeveel bolletjes wilt u? "))
+            return(bolletje)
+        
+        except ValueError:
+            print("Sorry, dat snap ik niet.")
+
+def kies_horen_of_bakje(bolletje):
+    while True:
+        if MIN_BOLLETJES <= bolletje <= MAX_BOLLETJES_HOORNTJE:
+            hoorntjeofBakje = input(f"Wilt u deze {bolletje} bolletjes in een hoorntje of bakje? ")
+            if hoorntjeofBakje == "hoorntje":
+                print(f"Hier is uw {bolletje} bolletjes in een hoorntje.")
+                break
+            elif hoorntjeofBakje == "bakje":
+                print(f"Hier is uw {bolletje} bolletjes in een bakje.")
+                break
+        if MAX_BOLLETJES_HOORNTJE <= bolletje <= MAX_BOLLETJES_BAKJE:
+            print(f"Hier is uw {bolletje} bolletjes in een bakje.")
+            break
+
+        elif bolletje > MAX_BOLLETJES_BAKJE:
+            print("Sorry, zulke grote bakken hebben we niet.")
+            bolletje = Vraag_bolletjes()
+            
+            
+        else:
+            print("Sorry, dat snap ik niet.")
+          
+
+
+def meerbestellen():
+    jofninput = input("Wilt u nog meer bestellen? (Ja/Nee) ")
+    if jofninput == "nee":
+        print("Bedankt en tot ziens!")
+        return("nee")
+
+    if jofninput == "ja":
+        return()
+    else:
+        print("Sorry, dat snap ik niet.")
+
+
+         
+    
